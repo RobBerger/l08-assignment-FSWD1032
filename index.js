947407c273f36180942fcfd70e81855a52190feb
berger <robertberger.dev@gmail.com>
2023-01-30 09:30:34 -0500

    ran npm start

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+let letters = ['w', 'e', ' ', 'a', 'r', 'e', ' ', 'B', 'S', 'T'];
+let words = createWords(letters);
+words.forEach(word => {
+    console.log(word);
+});
+function createWords(letters) {
+    let temp = "";
+    let result = [];
+    letters.forEach(ltr => {
+        if (ltr != ' ') {
+            temp += ltr;
+        }
+        else {
+            result.push(temp);
+            temp = "";
+        }
+    });
+    return result;
+}
+//# sourceMappingURL=index.js.map
