50d079a75e34369473a57aa402a1c0f16c362d65
berger <robertberger.dev@gmail.com>
2023-01-30 09:13:10 -0500

    added the code to index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+let letters = [ 'w', 'e', ' ', 'a', 'r', 'e', ' ', 'B', 'S', 'T' ];
+
+let words: string[] = createWords(letters);
+
+words.forEach(word => {
+    console.log(word);
+});
+
+function createWords(letters: string[]): string[]
+{
+    let temp = "";
+    let result: string[] = [];
+
+    letters.forEach(ltr => {
+        if (ltr != ' ') {
+            temp += ltr;
+        } else {
+            result.push(temp);
+            temp = "";
+        }
+    });
+
+    return result;
+}
